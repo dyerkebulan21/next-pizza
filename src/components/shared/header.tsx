c587bb73f9085c442ca2312c62f74b1685ad47a1
dyerkebulan21 <dyerkebulan21@gmail.com>
2024-06-27 22:43:44 +0500

    add container and header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   className?: string;
 }
 
 export const Header: React.FC<Props> = ({ className }) => {
-  return <div>header</div>;
+  return (
+    <header className={cn("border border-b", className)}>
+      <div></div>
+    </header>
+  );
 };
